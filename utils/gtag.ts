4f8bf5ec1b815110f3f9dad399c75dd7a0e4e885
Hany Morcos <hanymorcos@hanys-mbp.myfiosgateway.com>
2022-07-27 14:58:21 -0400

    Add gtag window fix

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -1,24 +1,28 @@
-export const GA_TRACKING_ID = "G-C4BR8LSDBE";
+export const GA_TRACKING_ID = 'G-C4BR8LSDBE'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url
-  });
-};
+  if (typeof window !== 'undefined') {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    })
+  }
+}
 
 type GTagEvent = {
-  action: string;
-  category: string;
-  label: string;
-  value: number;
-};
+  action: string
+  category: string
+  label: string
+  value: number
+}
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value
-  });
-};
+  if (typeof window !== 'undefined') {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    })
+  }
+}
