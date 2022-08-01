173b08b85f772434b290b7fa406c0d9d88eb678a
Hany Morcos <hanymorcos@hanys-mbp.myfiosgateway.com>
2022-08-01 13:30:12 -0400

    add nextjs recommendation

--- next.config.js
+++ next.config.js
@@ -55,7 +55,7 @@ export default withContentlayer()(
       }
 
       // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-      config.experiments = { asyncWebAssembly: true }
+      config.experiments = { ...config.experiments, asyncWebAssembly: true }
 
       return config
     },
